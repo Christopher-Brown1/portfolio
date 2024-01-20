04d8fcbc5d7a03a8c4921b262398d3f89cd340f2
Jameson Brown <jamesonbrown@neolivity.com>
2024-01-20 11:03:59 -0700

    Updated menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -11,110 +11,120 @@ import LinkedInIcon from "../assets/shared/linkedin.svg";
 export const Menu = (props) => {
   const location = useLocation();
   const navigate = useNavigate();
+  const project = location.pathname.split("/").slice(-1)[0];
 
   const menuText = () => {
-    switch (location.pathname) {
-      case "/project/1":
+    switch (project) {
+      case "1":
         return "Survivor Companion App";
-      case "/project/2":
+      case "2":
         return "Apple Music - New Features";
-      case "/project/3":
+      case "3":
         return "Goodreads Website Redesign";
       default:
         return "Home";
     }
   };
-  return props.isOpen ? (
-    <div
-      className="w-screen h-screen fixed left-0"
-      onClick={() => props.setIsOpen(false)}
-    >
-      <div
-        className="absolute bg-[#F8F5F1] border-[3px] border-[#2A2E2C] w-full max-w-[477px] p-[16px] z-10 sm:right-[64px]"
-        onClick={(e) => e.stopPropagation()}
-      >
-        <div className="flex justify-between items-center border-b-[1px] border-[#387C6D] px-[16px] pb-[16px]">
-          <button
-            onClick={(e) => {
-              e.stopPropagation();
-              props.setIsOpen(false);
-              navigate("/");
-            }}
+  return (
+    <React.Fragment>
+      {props.isOpen && (
+        <div
+          className="w-screen h-screen fixed left-0"
+          onClick={() => props.setIsOpen(false)}
+        >
+          <div
+            className="absolute bg-[#F8F5F1] border-[3px] border-[#2A2E2C] w-full max-w-[477px] p-[16px] z-10 top-[104px] sm:right-[64px]"
+            onClick={(e) => e.stopPropagation()}
           >
-            <img alt="home button" src={HomeIcon} />
-          </button>
-          <p className="text-[48px] font-megrim">CB</p>
-          <button
-            onClick={(e) => {
-              e.stopPropagation();
-              props.setIsOpen(false);
-            }}
-          >
-            <img alt="close menu button" src={CloseIcon} />
-          </button>
+            <div className="flex justify-between items-center border-b-[1px] border-[#387C6D] px-[16px] pb-[16px]">
+              <button
+                onClick={(e) => {
+                  e.stopPropagation();
+                  props.setIsOpen(false);
+                  navigate("/");
+                }}
+              >
+                <img alt="home button" src={HomeIcon} />
+              </button>
+              <p className="text-[48px] font-megrim">CB</p>
+              <button
+                onClick={(e) => {
+                  e.stopPropagation();
+                  props.setIsOpen(false);
+                }}
+              >
+                <img alt="close menu button" src={CloseIcon} />
+              </button>
+            </div>
+            <div className="p-[32px]">
+              <button
+                className={`text-[#2A2E2C80] text-[19px] font-montserrat pb-[16px] text-right w-full${
+                  project === "1" ? " font-[900]" : " font-[400]"
+                }`}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  props.setIsOpen(false);
+                  navigate(`project/1`);
+                }}
+              >
+                Survivor Companion App
+              </button>
+              <button
+                className={`text-[#2A2E2C80] text-[19px] font-montserrat pb-[16px] text-right w-full${
+                  project === "2" ? " font-[900]" : " font-[400]"
+                }`}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  props.setIsOpen(false);
+                  navigate(`project/2`);
+                }}
+              >
+                Apple Music - New Features
+              </button>
+              <button
+                className={`text-[#2A2E2C80] text-[19px] font-montserrat text-right w-full${
+                  project === "3" ? " font-[900]" : " font-[400]"
+                }`}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  props.setIsOpen(false);
+                  navigate(`project/3`);
+                }}
+              >
+                Goodreads Website Redesign
+              </button>
+            </div>
+            <div className="flex justify-between items-center border-t-[1px] border-[#387C6D] px-[16px] pt-[16px]">
+              <a
+                href={`${process.env.PUBLIC_URL}/resume.pdf`}
+                rel="noreferrer"
+                target="_blank"
+              >
+                <img alt="home button" src={ResumeIcon} />
+              </a>
+              <a href="mailto: chrisbrownuiux@gmail.com">
+                <img alt="close menu button" src={EmailIcon} />
+              </a>
+              <a href="https://www.linkedin.com/in/chrisbrownuiux/">
+                <img alt="close menu button" src={LinkedInIcon} />
+              </a>
+            </div>
+          </div>
         </div>
-        <div className="p-[32px]">
-          <button
-            className="text-[#2A2E2C80] text-[19px] font-montserrat font-[400] pb-[16px] text-right w-full"
-            onClick={(e) => {
-              e.stopPropagation();
-              props.setIsOpen(false);
-              navigate(`project/1`);
-            }}
-          >
-            Survivor Companion App
-          </button>
-          <button
-            className="text-[#2A2E2C80] text-[19px] font-montserrat font-[400] pb-[16px] text-right w-full"
-            onClick={(e) => {
-              e.stopPropagation();
-              props.setIsOpen(false);
-              navigate(`project/2`);
-            }}
-          >
-            Apple Music - New Features
-          </button>
-          <button
-            className="text-[#2A2E2C80] text-[19px] font-montserrat font-[400] text-right w-full"
-            onClick={(e) => {
-              e.stopPropagation();
-              props.setIsOpen(false);
-              navigate(`project/3`);
-            }}
-          >
-            Goodreads Website Redesign
-          </button>
-        </div>
-        <div className="flex justify-between items-center border-t-[1px] border-[#387C6D] px-[16px] pt-[16px]">
-          <a
-            href={`${process.env.PUBLIC_URL}/resume.pdf`}
-            rel="noreferrer"
-            target="_blank"
-          >
-            <img alt="home button" src={ResumeIcon} />
-          </a>
-          <a href="mailto: chrisbrownuiux@gmail.com">
-            <img alt="close menu button" src={EmailIcon} />
-          </a>
-          <a href="https://www.linkedin.com/in/chrisbrownuiux/">
-            <img alt="close menu button" src={LinkedInIcon} />
-          </a>
-        </div>
-      </div>
-    </div>
-  ) : (
-    <button
-      className="flex items-center font-montserrat text-[14px] sm:text-[19px] text-[#2a2e2c80] menuButton"
-      onClick={() => props.setIsOpen(true)}
-    >
-      {menuText()}{" "}
-      <img
-        alt="open menu"
+      )}
+      <button
+        className="flex items-center font-montserrat text-[14px] sm:text-[19px] text-[#2a2e2c80] menuButton"
         onClick={() => props.setIsOpen(true)}
-        className="ml-[8px] menuButton"
-        src={MenuArrow}
-      />
-    </button>
+      >
+        {menuText()}{" "}
+        <img
+          alt="open menu"
+          onClick={() => props.setIsOpen(true)}
+          className="ml-[8px] menuButton"
+          src={MenuArrow}
+        />
+      </button>
+    </React.Fragment>
   );
 };
 
