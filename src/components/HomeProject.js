963ad7b5c3912ebbf1185c3b3212d3271eaa6a5a
Jameson Brown <jamesonbrown@neolivity.com>
2024-03-21 22:42:34 -0600

    Fixed spacing on mobile

--- src/components/HomeProject.js
+++ src/components/HomeProject.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React from "react"
+import { useNavigate } from "react-router-dom"
 
 export const HomeProject = (props) => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   return (
     <div
@@ -16,7 +16,7 @@ export const HomeProject = (props) => {
           props.i === 1 ? "sm:mr-[4%]" : "ml-[4%]"
         }`}
       >
-        <h4 className="mb-[64px] text-[36px] text-[#343F56] font-montserrat">
+        <h4 className="mb-[64px] mt-[32px] sm:mt-0 text-[36px] text-[#343F56] font-montserrat">
           {props.title}
         </h4>
         <p className="text-[$343F56] mb-[16px] text-left">
@@ -33,7 +33,7 @@ export const HomeProject = (props) => {
         </button>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default HomeProject;
+export default HomeProject
